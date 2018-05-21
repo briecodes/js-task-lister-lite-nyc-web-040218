e458ea00b2adc00ef2c04c8ba12458b5fc758e8d
briecodes <hello@brie.work>
2018-05-21 15:38:32 -0400

    added features.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let input = document.getElementById('new-task-description')
+let input = document.getElementById('new-task-description');
+let priority = document.getElementById('priority');
 
 document.getElementById('create-task-form').addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(input.value.length + ". " + input.value)
     if (input.value.length > 0 && input.value !== " "){
         let newLi = document.createElement('li')
         newLi.innerText = input.value;
@@ -12,8 +12,22 @@ document.getElementById('create-task-form').addEventListener('submit', function(
         newDelete.innerText = "X";
         newDelete.setAttribute("class", "deleteMe");
         newLi.appendChild(newDelete);
+        if (priority.value){
+            newLi.setAttribute("class", priority.value);
+        }
+        // ROGER’S CODE
+        // button.setAttribute("onclick","deleteFunction(this)")
+        // SHUN’S CODE
+        // const button = document.createElement('button');
+        // button.addEventListener("click", () => {
+        //     li.remove();
+        // });
     }
 })
+
+// function deleteFunction(sender){
+//     sender.parentNode.remove()
+// }
 
 document.addEventListener('click', function(e){
     if (e.target.classList.contains("deleteMe")){
